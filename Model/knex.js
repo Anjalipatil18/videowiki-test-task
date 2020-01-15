53841accb0ebc8dce5edb68f0596e0e9ee0db228
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-15 14:26:49 +0530

    made folder and connection file using mysql using mvc

---
+++ Model/knex.js
@@ -0,0 +1,10 @@
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'Anjali2018@',
+        database: 'Turing'
+    }
+})
+module.exports = knex;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const socketIO = require('socket.io'); 
+// const socketIO = require('socket.io')(http); 
+// console.log(socketIO)
 const express = require('express')
-const app =express()
+const app = express()
 app.use(express.json())
 
 app.listen(3000,()=>{
     console.log("server is listening port number 3000.....")
 })
+// console.log("server is listening port number 3000.....")

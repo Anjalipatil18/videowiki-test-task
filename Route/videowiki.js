c37d54087c792e2ced545ab960a3e320e84be531
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-17 10:52:14 +0530

    in login route i am doing some changes

--- Route/videowiki.js
+++ Route/videowiki.js
@@ -12,30 +12,70 @@ videowiki.post('/register',(req,res)=>{
     }
     let response = postvideowiki.insertDetails(userDetails)
     response.then((result)=>{
-        console.log(result)
-        let token=jwt.sign({"user":result},'Anjali')
-        res.cookie(token)
         return res.json(result);
     }).catch((err)=>{
         res.send(err)
     });
 });
 
-videowiki.post('/login',(req,res)=>{
-    let email=req.body.email 
-    let password=req.body.password 
+// videowiki.post('/login',(req,res)=>{
+//     let email=req.body.email 
+//     let password=req.body.password 
+//     let response = postvideowiki.login()
+//     response.then((result)=>{
+//         for(let i=0; i<result.length; i++){
+//             if((result[i]["email"]==email) && (result[i]["password"]==password)){
+//                 let token=jwt.sign({"user":result},'Anjali')
+//                 res.cookie(token)
+//                 jwt.verify(token, 'Anjali', (err,data) => {
+//                     res.send(data)
+//                 })
+//             }
+//         }
+//     })  
+// });
+
+
+videowiki.post("/login",(req,res)=>{
+    const user={
+        email:req.body.email ,
+        password:req.body.password 
+    }
     let response = postvideowiki.login()
-    console.log(response)
     response.then((result)=>{
-        console.log(result)
         for(let i=0; i<result.length; i++){
-            if((result[i]["email"]==email) && (result[i]["password"]==password)){
-                let token=jwt.sign({"user":result},'Anjali')
-                res.cookie(token)
-                res.send("It's correct!")
+            if((result[i]["email"]==user.email)&&(result[i]["password"]==user.password)){
+                jwt.sign(user,"SECRET_KEY",(err,token)=>{
+                    res.cookie(token)
+                    res.json({
+                        token:token
+                    })
+                })
+            }else{
+                res.json({"status":"wrong","massage":"Your password Or Email wrong"})
             }
         }
-    })  
+    })
+    
+})
+
+// Token verification function.
+videowiki.post('/api/posts',(req,res)=>{
+    const bearerHeader = req.headers.authorization;
+    console.log(bearerHeader)
+    const bearerToken = bearerHeader.split();
+    // console.log(bearerToken)
+    req.token=bearerToken; 
+    jwt.verify((req.token),"SECRET_KEY",(authData,err)=>{
+       if(authData){
+            res.json({
+            message:"post created....",authData
+        });
+       }
+    else {
+        res.sendStatus(403);
+    }
+    })
 });
 
 module.exports=videowiki

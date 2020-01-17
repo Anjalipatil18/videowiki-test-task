942f546b12c0966fbb25be2abc9e2d109fb798cb
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-17 14:39:42 +0530

    in login route mostly done but one error is comming

--- Route/videowiki.js
+++ Route/videowiki.js
@@ -18,34 +18,33 @@ videowiki.post('/register',(req,res)=>{
     });
 });
 
-videowiki.post("/error",(req,res,next)=>{
+videowiki.get("/login",(req,res)=>{
     const user={
                 email:req.body.email ,
                 password:req.body.password 
             }
-    jwt.sign(user,"SECRET_KEY",(err,token)=>{
-        res.json({
-            token:"Bearer " + token
-        })
+    let response=postvideowiki.login()
+    response.then((result)=>{
+        for(let i=0; i<result.length; i++){
+            if((result[i]["email"]==user.email) && (result[i]["password"]==user.password)){
+                jwt.sign(user,"SECRET_KEY",(err,token)=>{
+                        res.json({
+                            token:token
+                        })
+                    })
+            }
+        }
+
+
     })
+    
 })
 
 ///Token verification function.
-function verifyToken(req,res,next){
+videowiki.get('/verification',(req,res)=>{
     const bearerHeader = req.headers.authorization;
-
-    if(typeof bearerHeader !== 'undefined'){
-        const bearerToken = bearerHeader.split(' ')[1];
-        req.token=bearerToken; 
-        next();
-
-    }else{
-       res.sendStatus(403);
-    }
-}
-
-videowiki.post('/api/posts',verifyToken,(req,res,next)=>{
-    jwt.verify((req.token),"SECRET_KEY",(err,authData)=>{
+    const bearerToken = bearerHeader.split(' ')[1];
+    jwt.verify((bearerToken),"SECRET_KEY",(err,authData)=>{
        if(err){
            res.sendStatus(403);
        }

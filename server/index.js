b191ad795437bf1f38d059453219786e904d56c7
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-27 17:12:47 +0530

    updated file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// console.log(express)
 const app = express()
-// console.log(app)
 app.use(express.json())
-// console.log(app.use(express.json()))
 
 const videowiki = require('../Route/videowiki')
 app.use("/videowiki",videowiki);
+
+const userInformation = require('../Route/userInformation')
+app.use("/",userInformation);
 
 app.listen(8081,()=>{
     console.log("server is listening port number 8081.....")

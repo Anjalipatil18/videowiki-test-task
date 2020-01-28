8eba7ae93dd87275c0095fa7a50a2dd91cfa9bfb
Anjalipatil18 <anjalip18@navgurukul.org>
2020-01-28 16:22:52 +0530

    create post by userid made

--- Model/createTable.js
+++ Model/createTable.js
@@ -1,25 +1,41 @@
 const knex=require("./knex")
 
-// knex.schema.createTable('post_likes', (table) => {
-//     table.increments('id')
-//     table.integer('user_id').unsigned();
-//     table.foreign('user_id').references('register.id')
-//     table.integer('post_id').unsigned();
-//     table.foreign('post_id').references('createPost.post_id')
-//     table.boolean('likes')
-//     table.string('comment')
+// knex.schema.createTable('register', (table) => {
+//     table.increments('user_id')
+//     table.string('username'),
+//     table.string('email').unique(), 
+//     table.string('password')
 // })
 
-knex.schema.createTable('userInformation',(table)=>{
+// knex.schema.createTable('createPost', (table) => {
+//     table.increments('post_id')
+//     table.integer('user_id').unsigned();
+//     table.foreign('user_id').references('register.user_id')    
+//     table.string('post')
+//     table.string('caption')
+// })
+
+knex.schema.createTable('post_likes', (table) => {
     table.increments('id')
-    table.string('Name')
-    table.integer('Birthday')
-    table.integer('Mobile')
-    table.string('Gender')
-    table.string('location')
-    table.string('education')
-    table.string('experience')
+    table.string('username')
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('register.user_id')
+    table.integer('post_id').unsigned();
+    table.foreign('post_id').references('createPost.post_id')
+    table.boolean('likes')
+    table.string('comment')
 })
+
+// knex.schema.createTable('userInformation',(table)=>{
+//     table.increments('id')
+//     table.string('Name')
+//     table.integer('Birthday')
+//     table.integer('Mobile')
+//     table.string('Gender')
+//     table.string('location')
+//     table.string('education')
+//     table.string('experience')
+// })
     .then(() => {
         console.log("table created")
     })
